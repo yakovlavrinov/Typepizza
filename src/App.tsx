f13f9ec99a4126a0641011db38353d2f01bc0003
Yakov Lavrinov <lawrinov.yackov@yandex.ru>
2023-12-25 21:55:48 +0300

    one

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./App.css";
-
+import AddPizzaForm from "./components/AddPizzaForm";
+import Pizza from "./models/Pizza";
 const App: FC = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+  const addPizza = (newPizza: Pizza) => {
+    setPizzasList([...pizzasList, newPizza]);
+  };
   return (
     <div className="App">
       <div className="wrap">
         <span className="heading">Наша пиццеря</span>
+        <AddPizzaForm addPizza={addPizza} />
       </div>
     </div>
   );
